b88a46eba720c755bc8fe6bf1b86948e78d68711
Em <emily30198@byui.edu>
2018-06-07 15:15:04 -0600

    fotter fixed

---
+++ cit230/assignments/lesson-7/scripts/current-date-7.js
@@ -0,0 +1,10 @@
+var dayName = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+var now = new Date();
+
+
+var monName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+var dtString = dayName[now.getDay()] + ", " + monName[now.getMonth()] + " " + now.getDate();
+
+document.getElementById("demo").innerHTML = dtString;
