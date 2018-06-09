4e37ac9a3de436dd2e0aececd4ccf6d5ea1406e2
Em <emily30198@byui.edu>
2018-06-09 11:52:45 -0600

    fixed github mess up

--- cit230/assignments/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/scripts/windchill.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var t = 50;
 var s= 100;
 
@@ -8,15 +7,4 @@ document.getElementById("temp").innerHTML=t;
 
 document.getElementById("windSpeed").innerHTML=s;
 
-=======
-var t = 50;
-var s= 100;
-
-var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275* t * Math.pow(s, 0.16); 
-
-document.getElementById("temp").innerHTML=t;
-
-document.getElementById("windSpeed").innerHTML=s;
-
->>>>>>> 2b6e4deec8f657668c40352b14726a391db7fcd5
 document.getElementById("windChill").innerHTML=f.toFixed(2);
