a52f6e2c47a2fd0828ddb4f8b331ab02f42d7c94
Em <emily30198@byui.edu>
2018-06-09 14:13:41 -0600

    javascript fixed

--- cit230/assignments/lesson-6/script/current-date.js
+++ cit230/assignments/lesson-6/script/current-date.js
@@ -1,9 +1,15 @@
 var dayName = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
 var now = new Date();
 
 
 var monName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
+
 var dtString = dayName[now.getDay()] + ", " + monName[now.getMonth()] + " " + now.getDate();
 
 document.getElementById("demo").innerHTML = dtString;
+
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("year").innerHTML = n;
--- cit230/assignments/lesson-7/scripts/current-date-7.js
+++ cit230/assignments/lesson-7/scripts/current-date-7.js
@@ -5,6 +5,11 @@ var now = new Date();
 
 var monName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
+
 var dtString = dayName[now.getDay()] + ", " + monName[now.getMonth()] + " " + now.getDate();
 
 document.getElementById("demo").innerHTML = dtString;
+
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("year").innerHTML = n;
