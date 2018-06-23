b374fc6031195f36f88e36bf01591a85126aef07
Em <emily30198@byui.edu>
2018-06-23 10:44:47 -0600

    push up not done forcast done index page

---
+++ cit230/assignments/lesson-9/scripts/forcast.js
@@ -0,0 +1,18 @@
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'http//api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=8928a9fd9cf320451b8f6d922f405929&units=imperial',true);
+
+weatherObject.send();
+
+weatherObject.onload = function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML = weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
+    
+    var iconcode = weatherInfo.weather[0].icon;
+    var icon_path = "http://openweathermao.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
+    
+}
